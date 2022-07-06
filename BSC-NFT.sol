75d63327fc0bde0589e7c31accdfe99ff3e20d20
RumeelHussainbnb <93580180+RumeelHussainbnb@users.noreply.github.com>
2022-07-07 00:11:36 +0500

    Update BSC-NFT.sol

--- BSC-NFT.sol
+++ BSC-NFT.sol
@@ -7,58 +7,66 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/** @title HelloWorld */ 
 contract BSCNFT is ERC721, ERC721Enumerable, Pausable, Ownable {
 
-    // ===== 1. Property Variables ===== //
+    /* Property Variables */
 
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    uint256 public MINT_PRICE = 0.05 ether;
-    uint public MAX_SUPPLY = 100;
+    uint256 public MINT_PRICE = 0.05 ether;  //Change this value as per your requirement
+    uint public MAX_SUPPLY = 100;            //Change this value as per your requirement
 
-    // ===== 2. Lifecycle Methods ===== //
-
+    /** @dev Constructor to set Name and Initials for NFT
+             and increment token counter 
+     */
     constructor() ERC721("HappyMonkey", "HM") {
         // Start token ID at 1. By default is starts at 0.
         _tokenIdCounter.increment();
     }
 
+    /** @dev Withdraw Tokens 
+     */
     function withdraw() public onlyOwner() {
         require(address(this).balance > 0, "Balance is zero");
         payable(owner()).transfer(address(this).balance);
     }
 
-    // ===== 3. Pauseable Functions ===== //
-
+    /** @dev Pause NFT Function
+      */
     function pause() public onlyOwner {
         _pause();
     }
-
+    /** @dev Unpause NFT Function
+      */
     function unpause() public onlyOwner {
         _unpause();
     }
 
-    // ===== 4. Minting Functions ===== //
+    /** @dev Function to Mint NFTs
+      */
 
     function safeMint(address to) public payable {
-        // ❌ Check that totalSupply is less than MAX_SUPPLY
+        // Check that totalSupply is less than MAX_SUPPLY
         require(totalSupply() < MAX_SUPPLY, "Can't mint anymore tokens.");
 
-        // ❌ Check if ether value is correct
+        // Check if enough amount of Ethers are passed
         require(msg.value >= MINT_PRICE, "Not enough ether sent.");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    // ===== 5. Other Functions ===== //
-
+    /** @dev Function to set the URI of the NFT
+      */
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://happyMonkeyBaseURI/";
+        return "ipfs://happyMonkeyBaseURI/";    //change this for your token
     }
 
+    /** @dev Sanity Checks before the token is transferrred
+      */
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         whenNotPaused
@@ -67,8 +75,8 @@ contract BSCNFT is ERC721, ERC721Enumerable, Pausable, Ownable {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
-
+    /** @dev The following functions are overrides required by Solidity.
+      */
     function supportsInterface(bytes4 interfaceId)
         public
         view
